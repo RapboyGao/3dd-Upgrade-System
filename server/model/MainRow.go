f04d63797945dba66430bdcd4589acccf3895698
RapboyGao <atgao74201@qq.com>
2025-07-24 18:01:25 +0800

    2025.07.24 refactor(model): 为MainRow结构体添加gorm列标签
    统一数据库列名与JSON字段名的映射关系，确保数据库操作时字段名一致

--- server/model/MainRow.go
+++ server/model/MainRow.go
@@ -11,79 +11,79 @@ type MainRow struct {
 	gorm.Model
 
 	// 员工号 主键
-	StaffID string `json:"staffID,omitempty"`
+	StaffID string `gorm:"primaryKey;column:staff_id" json:"staffID,omitempty"`
 
 	// 员工姓名
-	Name string `json:"name,omitempty"`
+	Name string `gorm:"column:name" json:"name,omitempty"`
 
 	// 员工姓名简拼
-	NameShortcut string `json:"nameShortcut,omitempty"`
+	NameShortcut string `gorm:"column:name_shortcut" json:"nameShortcut,omitempty"`
 
 	// 员工姓名拼音
-	NamePinYin string `json:"namePinYin,omitempty"`
+	NamePinYin string `gorm:"column:name_pinyin" json:"namePinYin,omitempty"`
 
 	// 当前行政部门
-	CurrentDepartment string `json:"currentDepartment,omitempty"`
+	CurrentDepartment string `gorm:"column:current_department" json:"currentDepartment,omitempty"`
 
 	// 在三大队时的最后一个行政部门
-	LastDepartment3dd string `json:"lastDepartment3dd,omitempty"`
+	LastDepartment3dd string `gorm:"column:last_department_3dd" json:"lastDepartment3dd,omitempty"`
 
 	// 行政部门历史
-	DepartmentHistory string `json:"departmentHistory,omitempty"`
+	DepartmentHistory string `gorm:"column:department_history" json:"departmentHistory,omitempty"`
 
 	// 遗忘飞过的机型，用json存储
-	AircraftExperiences string `json:"AircraftExperiences,omitempty"`
+	AircraftExperiences string `gorm:"column:aircraft_experiences" json:"AircraftExperiences,omitempty"`
 
 	// 最高的飞行标准
-	HighestLevel api.SpecificLevel `json:"highestLevel,omitempty"`
+	HighestLevel api.SpecificLevel `gorm:"column:highest_level" json:"highestLevel,omitempty"`
 
 	// F1升级日期时间戳
-	F1 float32 `json:"F1,omitempty"`
+	F1 float32 `gorm:"column:f1" json:"F1,omitempty"`
 
 	// F2升级日期时间戳
-	F2 float32 `json:"F2,omitempty"`
+	F2 float32 `gorm:"column:f2" json:"F2,omitempty"`
 
 	// F2b升级日期时间戳
-	F2b float32 `json:"F2b,omitempty"`
+	F2b float32 `gorm:"column:f2b" json:"F2b,omitempty"`
 
 	// F3升级日期时间戳
-	F3 float32 `json:"F3,omitempty"`
+	F3 float32 `gorm:"column:f3" json:"F3,omitempty"`
 
 	// F3b升级日期时间戳
-	F3b float32 `json:"F3b,omitempty"`
+	F3b float32 `gorm:"column:f3b" json:"F3b,omitempty"`
 
 	// F4升级日期时间戳
-	F4 float32 `json:"F4,omitempty"`
+	F4 float32 `gorm:"column:f4" json:"F4,omitempty"`
 
 	// F5升级日期时间戳
-	F5 float32 `json:"F5,omitempty"`
+	F5 float32 `gorm:"column:f5" json:"F5,omitempty"`
 
 	// M升级日期时间戳
-	M float32 `json:"M,omitempty"`
+	M float32 `gorm:"column:m" json:"M,omitempty"`
 
 	// J升级日期时间戳
-	J float32 `json:"J,omitempty"`
+	J float32 `gorm:"column:j" json:"J,omitempty"`
 
 	// A1升级日期时间戳
-	A1 float32 `json:"A1,omitempty"`
+	A1 float32 `gorm:"column:a1" json:"A1,omitempty"`
 
 	// A2升级日期时间戳
-	A2 float32 `json:"A2,omitempty"`
+	A2 float32 `gorm:"column:a2" json:"A2,omitempty"`
 
 	// A2b升级日期时间戳
-	A2b float32 `json:"A2b,omitempty"`
+	A2b float32 `gorm:"column:a2b" json:"A2b,omitempty"`
 
 	// Ta升级日期时间戳
-	Ta float32 `json:"Ta,omitempty"`
+	Ta float32 `gorm:"column:ta" json:"Ta,omitempty"`
 
 	// Tb升级日期时间戳
-	Tb float32 `json:"Tb,omitempty"`
+	Tb float32 `gorm:"column:tb" json:"Tb,omitempty"`
 
 	// C升级日期时间戳
-	C float32 `json:"C,omitempty"`
+	C float32 `gorm:"column:c" json:"C,omitempty"`
 
 	// Tc升级日期时间戳
-	Tc float32 `json:"Tc,omitempty"`
+	Tc float32 `gorm:"column:tc" json:"Tc,omitempty"`
 }
 
 func (m MainRow) GetAircraftExperiences() ([]api.AircraftType, error) {
